eb7ab882f6218730f633e52e4c58693c10f16ba3
yasmeen123456789 <yasmeenyassu3@gmail.com>
2023-06-13 22:26:21 +0530

    first commit

---
+++ app1/urls.py
@@ -0,0 +1,8 @@
+app_name= 'anything'
+from django.urls import path
+from app1.views import*
+
+urlpatterns =[
+    
+path ('yasmeen/',yasmeen,name='yasmeen'),
+]
---
+++ app2/urls.py
@@ -0,0 +1,8 @@
+app_name='somethig'
+from django.urls import path
+from app2.views import*
+
+urlpatterns = [
+    
+path('virat/',virat, name ='virat'),
+]
